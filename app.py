eab4a5404ef9ad1bdd1de2bc9efa4a96e143f1d0
James <mirageapache@gmail.com>
2025-05-31 15:47:06 +0800

    [feat]新增文章功能

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ import os
 from config.settings import db
 from flask_migrate import Migrate
 import models
+from flask import flash
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
+app.secret_key = os.getenv("APP_SECRET_KEY")
 
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
 app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{ROOT_PATH}/db/blog.db"
@@ -26,9 +30,11 @@ def create():
     title = request.form.get("title")
     content = request.form.get("content")
 
-    print(title, content)
+    post = models.Post(title=title, content=content)
+    db.session.add(post)
+    db.session.commit() # 寫入資料庫
 
-    # create post in db
+    flash("文章已新增")
     return redirect(url_for("index"))
 
 
