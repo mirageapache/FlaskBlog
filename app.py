d18c22a5170e64d5b6048cce0516a5160c79df58
James <mirageapache@gmail.com>
2025-05-31 14:30:55 +0800

    [feat]建立"新增文章"介面、路由

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/post')
 def index():
-    return render_template('post/index.html.jinja')
+    return render_template('posts/index.html.jinja')
+
+@app.route("/posts/new")
+def new():
+    return render_template("posts/new.html.jinja")
+
+@app.route("/posts/create", methods=["POST"])
+def create():
+    title = request.form.get("title")
+    content = request.form.get("content")
+
+    print(title, content)
+
+    # create post in db
+    return redirect(url_for("index"))
+
 
 if __name__ == '__main__':
     app.run(port=5050, debug=True)
